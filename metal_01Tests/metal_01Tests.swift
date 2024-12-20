12aa8167d8ec43914f45a2b4f2574b28589a0e52
Jeremy Thorne <jez@jezandbecks.co.uk>
2024-12-20 10:51:10 +0000

    Initial Commit

---
+++ metal_01Tests/metal_01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  metal_01Tests.swift
+//  metal_01Tests
+//
+//  Created by Jeremy Thorne on 20/12/2024.
+//
+
+import Testing
+@testable import metal_01
+
+struct metal_01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
